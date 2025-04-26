6fce903324bf23eda381ae2da5b1698d5a039f1d
Hiruni Ranasinghe <ranasinghehiruni14@gmail.com>
2025-04-26 17:15:53 +0530

    create add details and show details

--- myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/AppController.java
@@ -1,8 +1,42 @@
 package lk.ac.vau.fas.ict.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lk.ac.vau.fas.ict.model.Student;
+
 @RestController
 @RequestMapping("app")
 public class AppController {
 
 	Student st1 = new Student("2020ICT103","Alice",25,"IT",3.8);
+	Student st2 = new Student("2020ICT40","Bob",24,"IT",3.4);
+	Student st3 = new Student("2020ICT87","Jane",23,"IT",2.9);
+	Student st4 = new Student("2020ICT09","Michel",25,"IT",3.1);
+	Student st5 = new Student("2020ICT05","Nick",22,"IT",3.0);
+	
+	//Student[] arr = {ob1,ob2,ob3,ob4};
+	/*@GetMapping("/list")
+	public Student[] getdetails() {
+		return arr;
+	}*/
+	
+	private Map<String,Student> mstudent = new HashMap<String,Student>();
+	
+	@GetMapping("/addDetails")
+	public void addobjects() {
+		mstudent.put(st1.getRegNo(),st1);
+		mstudent.put(st2.getRegNo(),st2);
+		mstudent.put(st3.getRegNo(),st3);
+		mstudent.put(st4.getRegNo(),st4);
+	}
+	
+	@GetMapping("/details")
+	public Map<String,Student> getdetails(){
+		return mstudent;
+	}
 }
